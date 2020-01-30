be243238eaf3a8d15b04b58f8d15a70dc1745f45
Ryan Beck <rb200@humboldt.edu>
2020-01-30 11:00:02 -0800

    finished Ball Roller

---
+++ Ball Roller/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int pointCount;
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        pointCount = 0;
+        winText.text = "";
+        setCountText();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            pointCount += 1;
+            setCountText();
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + pointCount.ToString();
+        if(pointCount >= 14)
+        {
+            countText.text = "";
+            winText.text = "You Win!!";
+        }
+    }
+}
